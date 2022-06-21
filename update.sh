25f75941e41ba734e7f25fca1773c989da1d1123
Mr.yao <lily02@rd.netease.com>
2022-06-21 20:31:15 +0800

    update

--- update.sh
+++ update.sh
@@ -4,17 +4,13 @@
 
 pwd;
 
-cd /Users/liluyao/Code/github.com/yaocoding007/weekly;
+cd-note;
 
 pwd;
 
 git add .
-git commit -m ${MSG}
+git commit -m "${MSG}"
 
 git push origin master -f
 
-echo "*********************************************"
-echo "*********************************************"
-echo "******* 笔记更新完成～ 又是努力(滑水)的一天 *******"
-echo "*********************************************"
-echo "*********************************************"
+echo -e "\033[36m 好记性不如烂笔头 \033[0m"
