55fb2a5b601369d1471bdde3117b962e32acaae9
Mr.yao <lily02@rd.netease.com>
2022-06-21 20:39:26 +0800

    update

--- update.sh
+++ update.sh
@@ -6,17 +6,15 @@ echo -e "\033[33m update msg: $MSG  \033[0m"
 
 echo -e "\n"
 echo -e "\033[33m dirname: `pwd`  \033[0m"
-
 echo -e "\n"
-
 cd /Users/liluyao/Code/github.com/yaocoding007/weekly;
 echo -e "\n"
-
 echo -e "\033[33m dirname: `pwd`  \033[0m"
-
+echo -e "\n"
 git add .
 git commit -m "${MSG}"
 
 git push origin master -f
 
+echo -e "\n"
 echo -e "\033[36m 好记性不如烂笔头 \033[0m"
