989ce31fd71c8adc05488c245287dd1255665a5b
Mr.yao <lily02@rd.netease.com>
2022-06-21 09:50:22 +0800

    自动提交脚本

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[[ $1 ]] && MSG=$1 || MSG="update"
+
+pwd;
+
+cd /Users/liluyao/Code/github.com/yaocoding007/weekly;
+
+pwd;
+
+git add .
+git commit -m ${MSG}
+
+git push origin master
+
+echo "笔记更新完成～ 又是努力(滑水)的一天"
